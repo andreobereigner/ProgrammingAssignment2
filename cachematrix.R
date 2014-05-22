5a0e110706748ec845cc0deb7f2feadea966efda
Andre Obereigner <andre@obereigner.de>
2014-05-22 17:51:57 +0200

    Completed Programming Assignment 2
    Completed Programming Assignment 2 of Coursera's Signature Track "R
    Programming".

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Application of R's Scooping Function
+#
+# The set of functions will calculate an inverse matrix only once and 
+# store the inversed matrix in cache for future access in order to avoid
+# the repeated (often time consuming) calculation of the same inverse matrix.
+#
+# Only when the original matrix changes will the functions calculate
+# the inversed matrix again.
+#
+##
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function stores and returns the original
+## and inversed matrix, depending on what method is being called.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  originalMatrix <- x
+  inversedMatrix <- NULL
+  
+  # Define the setter method and stores the original matrix
+  set <- function(y) {
+    originalMatrix <<- y
+    inversedMatrix <<- NULL
+  }
+  
+  # Define the getter method and returns the original matrix
+  get <- function() originalMatrix
+  
+  # Define the setter method and sets the inversed matrix
+  setInverseMatrix <- function(inverseMatrix) {
+    inversedMatrix <<- inverseMatrix
+  }
+  
+  # Defines the getter method and returns the inversed matrix
+  getInverseMatrix <- function() inversedMatrix
+  
+  list(set = set, get = get, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function checks whether the submitted 
+## variable already contains an inversed matrix and returns it if TRUE.
+#  When no inversed matrix is available, the method will extract
+# the original matrix, create a inverse matrix, store it and return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # Assume that x already ontains the inversed matrix
+  # and extract/store the inversed matrix
+  inversedMatrix <- x$getInverseMatrix()
+  
+  # Check if an inversed matrix was indeed returned
+  # (if not the variable will be empty/NULL),
+  # and return the inversed matrix
+  if(!is.null(inversedMatrix)) {
+    message("getting cached data")
+    return(inversedMatrix)
+  }
+  
+  # If an inversed matrix does not yet exist,
+  # extract the original matrix, create inversed matrix
+  # and store it. At last, return the inversed matrix.
+  data <- x$get()
+  inversedMatrix <- solve(data, ...)
+  x$setInverseMatrix(inversedMatrix)
+  inversedMatrix
+  
 }
